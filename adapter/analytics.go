dcfe1cb0e7dc6535ae2865b5275f7b2001c3036b
limit7412 <qazx7412@newgenerations.im>
2019-06-20 16:36:32 +0900

    Get analytics

--- adapter/analytics.go
+++ adapter/analytics.go
@@ -1,6 +1,15 @@
 package adapter
 
+import (
+	"context"
+	"os"
+
+	"google.golang.org/api/analytics/v3"
+)
+
 type AnalyticsAdapter interface {
+	GetAnalytics() (*analytics.GaData, error)
+	GetService() (*analytics.Service, error)
 }
 
 type analyticsImpl struct {
@@ -9,4 +18,31 @@ type analyticsImpl struct {
 // NewAnalyticsImplAdapter access to analytics
 func NewAnalyticsImplAdapter() AnalyticsAdapter {
 	return &analyticsImpl{}
+}
+
+func (a *analyticsImpl) GetAnalytics() (*analytics.GaData, error) {
+	service, err := a.GetService()
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := service.Data.Ga.
+		Get("ga:"+os.Getenv("PROFILE_ID"), "yesterday", "today", "ga:sessions").
+		Dimensions("ga:pagePath,ga:pagePathLevel1,ga:pagePathLevel2,ga:pageTitle").
+		Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (a *analyticsImpl) GetService() (*analytics.Service, error) {
+	ctx := context.Background()
+	analyticsService, err := analytics.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return analyticsService, nil
 }
