8c292c3bd7e42f66f42dbb57b6abb03e544b84b3
limit7412 <qazx7412@newgenerations.im>
2019-07-19 16:22:27 +0900

    Refactoring

--- repository/analytics.go
+++ repository/analytics.go
@@ -3,13 +3,14 @@ package repository
 import (
 	"context"
 	"os"
+	"sort"
+	"strconv"
 
 	"google.golang.org/api/analytics/v3"
 )
 
 type AnalyticsRepository interface {
-	GetSessions(start string, end string) (*analytics.GaData, error)
-	GetService() (*analytics.Service, error)
+	GetSessions(start string, end string) ([][]string, error)
 }
 
 type analyticsImpl struct {
@@ -20,24 +21,7 @@ func NewAnalyticsRepository() AnalyticsRepository {
 	return &analyticsImpl{}
 }
 
-func (a *analyticsImpl) GetSessions(start string, end string) (*analytics.GaData, error) {
-	service, err := a.GetService()
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := service.Data.Ga.
-		Get("ga:"+os.Getenv("PROFILE_ID"), start, end, "ga:sessions").
-		Dimensions("ga:pageTitle,ga:pagePath").
-		Do()
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func (a *analyticsImpl) GetService() (*analytics.Service, error) {
+func (a *analyticsImpl) getService() (*analytics.Service, error) {
 	ctx := context.Background()
 	analyticsService, err := analytics.NewService(ctx)
 	if err != nil {
@@ -45,4 +29,28 @@ func (a *analyticsImpl) GetService() (*analytics.Service, error) {
 	}
 
 	return analyticsService, nil
+}
+
+func (a *analyticsImpl) GetSessions(start string, end string) ([][]string, error) {
+	service, err := a.getService()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := service.Data.Ga.
+		Get("ga:"+os.Getenv("PROFILE_ID"), start, end, "ga:sessions").
+		Dimensions("ga:pageTitle,ga:pagePath").
+		Do()
+	if err != nil {
+		return nil, err
+	}
+
+	result := data.Rows
+	sort.Slice(result, func(i, j int) bool {
+		a, _ := strconv.Atoi(result[i][2])
+		b, _ := strconv.Atoi(result[j][2])
+		return a > b
+	})
+
+	return result, nil
 }
